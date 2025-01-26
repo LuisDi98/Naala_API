241c15f0f74a3085b7766fcea3d2661132771a24
LuisDi98 <aguilarluisdi@gmail.com>
2025-01-26 12:59:17 -0600

    refactor: remove custom CORS headers and options middleware in app.ts

--- src/app.ts
+++ src/app.ts
@@ -18,15 +18,6 @@ app.use(cors({
   credentials: true,
 }));
   
-app.options("*", cors());  
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://www.urbania-custom.com");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
-
 app.use(express.json());
 app.use('/api/pins', pinRoutes);
 app.use('/api/docx', docxRoutes);
