473891ed837724abc878e07656926374accd657f
LuisDi98 <aguilarluisdi@gmail.com>
2025-01-26 15:18:31 -0600

    refactor: update CORS handling and simplify app configuration in app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,28 +9,16 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
-// Asegurarse de que el preflight request está bien manejado
-app.options("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.urbania-custom.com");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(204);
-});
-
-app.use(cors({
-  origin: ["https://www.urbania-custom.com", "http://localhost:5173"], // Producción y desarrollo
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
-
-
-
-  
 app.use(express.json());
+app.use(cors());
+
+
 app.use('/api/pins', pinRoutes);
 app.use('/api/docx', docxRoutes);
+app.use("/",
+    (req, res)=>{
+        res.json({msg:"API de prueba"})
+    }
+)
 
 export default app;
