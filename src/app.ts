a8b06baeab656a25f14d62010a55b5ec7e0ec768
LuisDi98 <aguilarluisdi@gmail.com>
2025-01-26 13:04:13 -0600

    feat: enhance CORS handling by adding OPTIONS method support in app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,24 @@ connectDB();
 
 const app = express();
 
+// Asegurarse de que el preflight request está bien manejado
+app.options("*", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://www.urbania-custom.com");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(204);
+});
 
 app.use(cors({
-  origin: ["https://www.urbania-custom.com", "http://localhost:5173"],  // Agregar producción y desarrollo
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: ["https://www.urbania-custom.com", "http://localhost:5173"], // Producción y desarrollo
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 }));
+
+
+
   
 app.use(express.json());
 app.use('/api/pins', pinRoutes);
